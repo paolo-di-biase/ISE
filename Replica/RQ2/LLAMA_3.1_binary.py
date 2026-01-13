c28ea39fb50a25efb6a3a3797201a394b2a06915
Paolo Di Biase <paolodibiase1@gmail.com>
2026-01-13 18:41:59 +0100

    fix

--- Replica/RQ2/LLAMA_3.1_binary.py
+++ Replica/RQ2/LLAMA_3.1_binary.py
@@ -169,11 +169,10 @@ training_arguments = TrainingArguments(
 
 training_arguments = SFTConfig(
     output_dir="./outputs",
-    max_seq_length=512,
     per_device_train_batch_size=2,
     gradient_accumulation_steps=4,
     num_train_epochs=1,
-    tokenizer=tokenizer,   # <-- QUI va il tokenizer
+    tokenizer=tokenizer,
 )
 
 trainer = SFTTrainer(
@@ -182,6 +181,7 @@ trainer = SFTTrainer(
     peft_config=peft_config,
     formatting_func=lambda x: x["prompt_text"],
     args=training_arguments,
+    max_seq_length=512,
 )
 
 
